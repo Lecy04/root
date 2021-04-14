f3ec37c1bb6948ae3c744f5ef9967b575d1005dc
Aydar <zolot674@mail.ru>
2021-04-14 21:01:30 +0500

    Second remove server

--- main/views.py
+++ main/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
     return render(request, "main/home.html")
 
 
-def about(request):
-    return render(request, 'main/about.html')
+def comparison(request):
+    return render(request, 'main/comparison.html')
 
 # Create your views here.
