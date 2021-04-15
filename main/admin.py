a44f29edcf713cca31fd9e2f9e4b42bf7d59d984
Aydar <zolot674@mail.ru>
2021-04-15 22:58:57 +0500

    Finally home

--- main/admin.py
+++ main/admin.py
@@ -2,6 +2,4 @@ from django.contrib import admin
 from .models import Task
 
 
-admin.site.register(Task)
-
 # Register your models here.
--- main/views.py
+++ main/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
+from .models import Task
 
 def index(request):
-    return render(request, "main/home.html")
+    return render(request, "main/home.html",)
 
 
 
